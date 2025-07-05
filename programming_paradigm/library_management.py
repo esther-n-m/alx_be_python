a866bb08a35b5e8f2a186e1cbbd171e27d955b43
esther-n-m <esthermutembei50@gmail.com>
2025-07-06 02:12:17 +0300

    Week 6 assignments

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,59 @@
+
+class Book:
+    """Represents a book with title, author, and check-out status."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as returned."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Return True if the book is not checked out."""
+        return not self._is_checked_out
+
+
+class Library:
+    """Manages a collection of Book objects."""
+
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Mark the book with the given title as checked out."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False
+
+    def return_book(self, title):
+        """Return a book with the given title to the library."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+        return False
+
+    def list_available_books(self):
+        """Print all books that are currently available."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
