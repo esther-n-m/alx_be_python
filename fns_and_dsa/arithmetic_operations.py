ad0c6524fc7278d8cc0b9e5d37f7fe4ab51d9ce0
esther-n-m <esthermutembei50@gmail.com>
2025-07-06 01:12:28 +0300

    checking if function perform operation exist

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,5 @@
 
 def perform_operation(num1: float, num2: float, operation: str):
-    """
-    Perform basic arithmetic operations.
-
-    Parameters:
-        num1 (float): The first number.
-        num2 (float): The second number.
-        operation (str): The operation to perform ('add', 'subtract', 'multiply', 'divide').
-
-    Returns:
-        float or str: The result of the operation, or an error message for invalid operations.
-    """
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -23,3 +12,4 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 / num2
     else:
         return "Error: Invalid operation"
+
